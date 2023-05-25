7f9419f0945b1464e8dfc24c0e3cc6fb54da9451
dhruv-203 <pateldhruv2460@gmail.com>
2023-05-25 17:11:48 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './nav';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import About from './about';
+import Contact from './contact';
+import Home from './Home';
+import Hooks from './hooks';
+import UseEffect from "./Hooks/UseEffect"
+import UseState from "./Hooks/UseState"
+import UseRef from "./Hooks/UseRef"
+import UseContext from "./Hooks/UseContext"
+import UseReducer from "./Hooks/UseReducer"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className='displayContainer' >
+
+        <Routes >
+          <Route exact path="/" element={<Home />}
+          ></Route>
+          <Route exact path="/about" element={<About />}> </Route>
+          <Route exact path='/contact' element={<Contact />}></Route>
+          <Route path='/hooks' element={<Hooks />}>
+            <Route exact path="UseEffect" element={<UseEffect />}></Route>
+            <Route exact path="UseRef" element={<UseRef />}> </Route>
+            <Route exact path='UseContext' element={<UseContext />}></Route>
+            <Route exact path='UseState' element={<UseState />}></Route>
+            <Route exact path='UseReducer' element={<UseReducer />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+    // <Display />
   );
 }
-
-export default App;
